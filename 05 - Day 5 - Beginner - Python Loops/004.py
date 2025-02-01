68037b765a6fbc3b3616016747ed6f5022c7ec82
Minhuz Taznim Himel <minhuztaznim@gmail.com>
2025-02-01 22:06:30 +0600

    Add game to sum numbers from 1 to 100 in Day 5 exercises

--- 05 - Day 5 - Beginner - Python Loops/004.py
+++ 05 - Day 5 - Beginner - Python Loops/004.py
@@ -1,3 +1,12 @@
 #Range function with 3 arguments (start, stop, step)
-for number in range(32, 57, 2):
-    print(number)
+# for number in range(32, 57, 2):
+#     print(number)
+
+
+#Game: Add all numbers from 1 to 100
+total = 0
+for number in range(1,101):
+    
+    total += number
+    
+print(total)
