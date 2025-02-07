53d977988713d36c635dd634063eeb9b730fbbe4
Minhuz Taznim Himel <minhuztaznim@gmail.com>
2025-02-07 23:56:20 +0600

    Add solution for the 3rd level of the maze in Reeborg's World

---
+++ 06 - Day 6 - Beginner - Python Functions & Karel/003.py
@@ -0,0 +1,25 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+# for step in range(6):
+#     jump()
+
+#This is the solution for the 3rd level of the maze in Reeborg's World.
+#The solution is to use a for loop to repeat the jump function 6 times.
+#The jump function is defined to move, turn left, move, turn right, move, turn right, move, and turn left.
+#The turn_right function is defined to turn left three times.
+#The jump function is then called 6 times using a for loop.
+#The result is that Reeborg moves in a zigzag pattern across the maze to the end.
+#The code is then executed in Reeborg's World to solve the maze.
