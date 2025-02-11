cde022af14950c6ec039fc08c758cf5fd1df63da
Minhuz Taznim Himel <minhuztaznim@gmail.com>
2025-02-12 00:08:47 +0600

    Add examples demonstrating indentation in Python

---
+++ 06 - Day 6 - Beginner - Python Functions & Karel/004.py
@@ -0,0 +1,35 @@
+#Indentation in Python 
+# Python uses indentation to define the structure of the code.
+# Indentation is the process of adding whitespace before the statement.
+# The number of whitespaces added before the statement is called indentation level.
+# The indentation level must be consistent throughout the code block.
+
+# Example 1
+if 5 > 2:
+    print("5 is greater than 2")
+# Example 2
+if 5 > 2:
+    print("5 is greater than 2")
+    print("Hello World")
+# Example 3
+if 5 > 2:
+    print("5 is greater than 2")
+print("Hello World")
+# Example 4
+if 5 > 2:
+    print("5 is greater than 2")
+    print("Hello World")
+print("Hello World")
+# Example 5
+if 5 > 2:
+    print("5 is greater than 2")
+    print("Hello World")
+    print("Hello World")
+print("Hello World")
+# Example 6
+if 5 > 2:
+    print("5 is greater than 2")
+    print("Hello World")
+    print("Hello World")
+    print("Hello World")
+print("Hello World")
